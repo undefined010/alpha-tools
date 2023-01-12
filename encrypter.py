a425732f445397e10d7f55fd1ba6047923dc8ccb
undefined010 <118000380+undefined010@users.noreply.github.com>
2023-01-12 17:26:13 +0200

    Update encrypter.py

--- encrypter.py
+++ encrypter.py
@@ -1,14 +1,40 @@
+import os
 import colorama
 from colorama import Fore as f
+from cryptography.fernet import Fernet
 from pyfiglet import figlet_format as ff
 
+colorama.init(autoreset=True)
 
 class Encrypt:
+
+    
+
     def __init__(self) -> None:
-        pass
-
+        print(f.YELLOW + ff("Encrypter 0.1"))
+        self.key =  Fernet.generate_key()
+        self.cipher_suite = Fernet(self.key)
+    
     def encrypt_string(self,string) -> str:
-        pass
+        return self.cipher_suite.encrypt(string)
 
-    def ecrypt_file(self) -> None:
-        pass
+    def decrypt_string(self,string) -> str:
+        return self.cipher_suite.decrypt(string)
+
+
+        
+
+    """
+    def ecrypt_file(self,fname) -> None:
+        dlist = os.listdir()
+        is_found = False
+
+        for __file__ in dlist:
+            if __file__ == fname:
+                is_found == True
+        
+        if not is_found:
+            print(f.RED + "file not found")
+
+        #continue later
+    """
