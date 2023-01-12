4a418504736ac69d84747429f633b76a467c6c61
undefined010 <118000380+undefined010@users.noreply.github.com>
2023-01-12 22:04:27 +0200

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -16,24 +16,36 @@ class Scan:
         self.open_ports = []
 
     def scan(self) -> None:
-        for port in range(1,65535):
-            s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-            socket.setdefaulttimeout(0.3)
-            r = s.connect_ex((self.ipv4 , port))
+        try:
+            for port in range(1,65535):
+                s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+                socket.setdefaulttimeout(0.3)
+                r = s.connect_ex((self.ipv4 , port))
             
-            if r == 0:
-                print(f.CYAN + f'Port {port} is ' + f.GREEN + 'open' + f.CYAN + f' in {self.ipv4}')
-                self.open_ports.append(port)
-            s.close()
+                if r == 0:
+                    print(f.CYAN + f'Port {port} is ' + f.GREEN + 'open' + f.CYAN + f' in {self.ipv4}')
+                    self.open_ports.append(port)
+                s.close()
+        except socket.error:
+            print(socket.error)
+        except KeyboardInterrupt:
+            print(KeyboardInterrupt)
 
     def is_open(self,port) -> bool:
-        s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
-        socket.setdefaulttimeout(0.3)
-        if s.connect_ex((self.ipv4 , port)) == 0:
-            s.close()
-            return True
-        else:
-            s.close()
+        try:
+            s = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+            socket.setdefaulttimeout(0.3)
+            if s.connect_ex((self.ipv4 , port)) == 0:
+                s.close()
+                return True
+            else:
+                s.close()
+                return False
+        except socket.error:
+            print(socket.error)
+            return False
+        except KeyboardInterrupt:
+            print(KeyboardInterrupt)
             return False
 
     def get_open_ports(self) -> list:
